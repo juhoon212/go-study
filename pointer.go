e3b697b89814a37a21897c69a714eb2ca4e6b8c8
juhoon212 <ws5501@naver.com>
2025-02-09 12:28:31 +0900

    pointer

--- pointer.go
+++ pointer.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func Pointer() {
 	b := *getPointer()
-	fmt.Println("Value is = ", b)
+	fmt.Println("Value b is = ", b)
+	fmt.Println("Value b address is = ", &b)
+
+	c := getPointer()
+	fmt.Println("Value c address is = ", c)
 }
 
 func getPointer() (myPointer *int) {
